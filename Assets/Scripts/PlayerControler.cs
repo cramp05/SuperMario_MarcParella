5929718429fab9507d5736250d96fa6cafe676f4
cramp05 <mparella@ctsformacion.com>
2025-12-10 13:55:52 +0100

    Movimiento

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,3 +1,4 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
@@ -6,15 +7,43 @@ public class PlayerControler : MonoBehaviour
 
     public float movementSpeed = 5;
     public int direction = 1;
+    public InputAction moveAction;
+    public Vector2 moveDirection;
+
+    public Rigidbody2D rBody2D;
+    public float jumpForce = 10;
+
+
+
+    void Awake()
+    {
+        rBody2D = GetComponent<Rigidbody2D>();
+    }
+
+
 
     void Start()
     {
+        transform.position = new Vector3(0, 0, 0);
+
         transform.position = startPosition;
+
+        moveAction = InputSystem.actions["Move"];
+
+        rBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + direction * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        moveDirection = moveAction.ReadValue<Vector2>();
+
+        //transform.position = new Vector3(transform.position.x + direction * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        //transform.position = vector2.MoveTowards(transform.position, new Vector2(transform.position.x + direction, transform.position.y), movementSpeed + Time.deltaTime);
+
+        //transform.Translate(new Vector3(moveDirection.x * movementSpeed * Time.deltaTime, 0, 0));
+        transform.position = new Vector3(transform.position.x + moveDirection.x * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+
+
+
     }
 }
